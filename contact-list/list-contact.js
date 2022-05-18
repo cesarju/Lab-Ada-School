d8c826522099d841c8ea981f192274d096c7eca0
Cesar Ticona <cesarju.cc@gmail.com>
2022-05-18 12:18:52 -0400

    refactor code to list contact with object

--- contact-list/list-contact.js
+++ contact-list/list-contact.js
@@ -1,20 +1,43 @@
-const contactList = [
+/* const contactList = [
   "DIEGO ALEJANDRO FORERO PEÑA",
   "CARLOS ANDRÉS POLO CASTELLANOS",
   "CAROL RUCHINA GOMEZ GIANINE",
   "DIANA CAROLINA LOPEZ RODRIGUEZ",
   "GABRIEL MAURICIO NIETO BUSTOS ",
-];
+]; */
+
+class contact {
+  constructor(id, nombres, apellidos, telefono, ciudad, direccion) {
+    (this.id = id),
+      (this.nombres = nombres),
+      (this.apellidos = apellidos),
+      (this.telefono = telefono),
+      (this.ubicacion = [(this.ciudad = ciudad), (this.direccion = direccion)]);
+  }
+}
+
+const contactList = [];
 // agragar un contacto a la lista
-const addContac = (nameContact) => {
-  contactList.push(nameContact);
+const addContac = (id, nombres, apellidos, telefono, ciudad, direccion) => {
+  const newContact = new contact(
+    id,
+    nombres,
+    apellidos,
+    telefono,
+    ciudad,
+    direccion
+  );
+  contactList.push(newContact);
 };
 // borrar un contacto de la lista
 const deleteContac = (arr, nameContact) => {
-  const position = arr.indexOf(nameContact);
-  if (position !== -1) {
-    arr.splice(position, 1);
-  }
+  const lista = [];
+  arr.find((element) => {
+    if (element.nombres !== nameContact) {
+      lista.push(element);
+    }
+  });
+  return lista;
 };
 
 //print contacs list
@@ -22,8 +45,12 @@ const printContacts = (list) => {
   console.log(list);
 };
 
-console.log(contactList[1]);
-addContac("CESAR TICONA");
+addContac("1", "Cesar", "Ticona", 71207497, "La Paz", "Alto tejar");
+addContac("2", "Adrian", "Flores", 69207497, "Cochabamba", "El Alto");
+addContac("3", "Luca", "Torres", 79017497, "La Paz", "Miraflores");
+addContac("4", "Marcelo", "Mamani", 77347497, "La Paz", "Obrajes");
+
 console.log(contactList);
-deleteContac(contactList, "CARLOS ANDRÉS POLO CASTELLANOS");
-printContacts(contactList);
+/* console.log(contactList1); */
+
+printContacts(deleteContac(contactList, "Luca"));
