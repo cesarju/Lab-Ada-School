1d5fb4fd2c8f696f986475d808995791be2d6418
Cesar Ticona <cesarju.cc@gmail.com>
2022-05-19 16:33:39 -0400

    Order contact list

--- contact-list/list-contact.js
+++ contact-list/list-contact.js
@@ -6,7 +6,7 @@
   "GABRIEL MAURICIO NIETO BUSTOS ",
 ]; */
 
-class contact {
+/* class contact {
   constructor(id, nombres, apellidos, telefono, ciudad, direccion) {
     (this.id = id),
       (this.nombres = nombres),
@@ -14,21 +14,15 @@ class contact {
       (this.telefono = telefono),
       (this.ubicacion = [(this.ciudad = ciudad), (this.direccion = direccion)]);
   }
-}
+} */
 
-const contactList = [];
+let contactList = [];
 // agragar un contacto a la lista
 const addContac = (id, nombres, apellidos, telefono, ciudad, direccion) => {
-  const newContact = new contact(
-    id,
-    nombres,
-    apellidos,
-    telefono,
-    ciudad,
-    direccion
-  );
+  const newContact = { id, nombres, apellidos, telefono, ciudad, direccion };
   contactList.push(newContact);
 };
+
 // borrar un contacto de la lista
 const deleteContac = (arr, nameContact) => {
   const lista = [];
@@ -37,8 +31,10 @@ const deleteContac = (arr, nameContact) => {
       lista.push(element);
     }
   });
-  return lista;
+  contactList = lista;
+  return contactList;
 };
+
 //update contact
 /* arr = lista de contactos
 nombreContact = busqueda del contacto a modificar
@@ -51,19 +47,33 @@ const updateContact = (arr, nombreContact, item, newItem) => {
     }
   });
 };
-
+//ordenamiento de la lista de contactos
+const ordenado = (arr) =>
+  arr.sort(function (a, b) {
+    if (a.nombres.toLowerCase() > b.nombres.toLowerCase()) {
+      return 1;
+    } else {
+      return -1;
+    }
+  });
 //print contacs list
 const printContacts = (list) => {
   console.log(list);
 };
+
 //Agregar contactos a la lista
 addContac("1", "Cesar", "Ticona", 71207497, "La Paz", "Alto tejar");
-addContac("2", "Adrian", "Flores", 69207497, "Cochabamba", "El Alto");
+addContac("2", "adrian", "Flores", 69207497, "Cochabamba", "El Alto");
+addContac("10", "Pedro", "Guerrero", 77707497, "Sucre", "Satelite");
 addContac("3", "Luca", "Torres", 79017497, "La Paz", "Miraflores");
 addContac("4", "Marcelo", "Mamani", 77347497, "La Paz", "Obrajes");
+addContac("5", "Angela", "Gutierrez", 60047497, "Beni", "Centro");
+
 // imprimir la lista mas borrar un contacto de la lista
-printContacts(deleteContac(contactList, "Luca"));
+deleteContac(contactList, "Marcelo");
 //actualizar un contacto
 updateContact(contactList, "Cesar", "nombres", "Julio");
 //imprime lista de contactos
-console.log(contactList);
+//console.log(contactList);
+
+console.log(ordenado(contactList));
