14a05427b8563053dc94d80d6fd5ec9a1228b113
Cesar Ticona <cesarju.cc@gmail.com>
2022-05-25 15:11:10 -0400

    Laboratori hooks-1

---
+++ react-hooks/src/components/Contador.js
@@ -0,0 +1,38 @@
+import "../App.css";
+import React from "react";
+import { useState, useEffect } from "react";
+export const Contador = () => {
+  const [counter, setCounter] = useState(0);
+  useEffect(() => {
+    console.log(`El contador va en cuenta ${counter}`);
+  }, [counter]);
+
+  return (
+    <div>
+      <h1>{counter}</h1>
+      <div className="container">
+        <button
+          onClick={() => {
+            setCounter(counter + 1);
+          }}
+        >
+          Aumentar
+        </button>
+        <button
+          onClick={() => {
+            setCounter(counter - 1);
+          }}
+        >
+          Disminuir
+        </button>
+        <button
+          onClick={() => {
+            setCounter(0);
+          }}
+        >
+          Resetaer
+        </button>
+      </div>
+    </div>
+  );
+};
